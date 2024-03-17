d9e4193bae12a4af0f13b03a671efefba5c24a91
PajarAland <162042636+PajarAland@users.noreply.github.com>
2024-03-18 02:05:35 +0700

    menambahkan class dataGeneric

--- TPMOD5_1302223146/Program.cs
+++ TPMOD5_1302223146/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMOD5_1302223146
+{
+    class DataGeneric<T>
+    {
+        T Data { get; set; }
+        public DataGeneric(T data)
+        {
+            Data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {Data}");
+        }
+    }
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            DataGeneric<string> dataString = new DataGeneric<string>("Y");
+            dataString.PrintData();
+            DataGeneric<string> dataNim = new DataGeneric<string>("1302223146");
+            dataNim.PrintData();
+            Console.ReadLine();
+        }
+    }
+}
